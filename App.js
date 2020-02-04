74fb973e92a8504ad0617fb21b7b1c7c07c6ce3b
iqbal choironi <iqbal.choironi@gmail.com>
2020-02-04 12:42:38 +0700

    Bottom navigation bagian 2

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import iconHome from "./icon/home-active.png";
+import iconOrder from "./icon/order.png";
+import iconHelp from "./icon/help.png";
+import iconInbox from "./icon/inbox.png";
+import iconAccount from "./icon/account.png";
 
 export default function App() {
   return (
@@ -9,27 +15,53 @@ export default function App() {
         <View
           style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
         >
-          <Text>Home</Text>
+          <Image style={{ width: 26, height: 26 }} source={iconHome} />
+          <Text
+            style={{
+              color: "#545454",
+              fontSize: 10,
+              marginTop: 4,
+              color: "green"
+            }}
+          >
+            Home
+          </Text>
         </View>
         <View
           style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
         >
-          <Text>Orders</Text>
+          <Image style={{ width: 26, height: 26 }} source={iconOrder} />
+
+          <Text style={{ color: "#545454", fontSize: 10, marginTop: 4 }}>
+            Orders
+          </Text>
         </View>
         <View
           style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
         >
-          <Text>Help</Text>
+          <Image style={{ width: 26, height: 26 }} source={iconHelp} />
+
+          <Text style={{ color: "#545454", fontSize: 10, marginTop: 4 }}>
+            Help
+          </Text>
         </View>
         <View
           style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
         >
-          <Text>Inbox</Text>
+          <Image style={{ width: 26, height: 26 }} source={iconInbox} />
+
+          <Text style={{ color: "#545454", fontSize: 10, marginTop: 4 }}>
+            Inbox
+          </Text>
         </View>
         <View
           style={{ alignItems: "center", justifyContent: "center", flex: 1 }}
         >
-          <Text>Account</Text>
+          <Image style={{ width: 26, height: 26 }} source={iconAccount} />
+
+          <Text style={{ color: "#545454", fontSize: 10, marginTop: 4 }}>
+            Account
+          </Text>
         </View>
       </View>
     </View>
