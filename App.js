b2180ad75ebab4599a617b7f7842531ccf4fd6dc
iqbal choironi <iqbal.choironi@gmail.com>
2020-02-06 23:05:13 +0700

    add goofood banner section

--- App.js
+++ App.js
@@ -39,6 +39,8 @@ import iconGomore from "./icon/go-more.png";
 import imageSepakBola from "./dummy/sepak-bola.jpg";
 import logoWhite from "./logo/white.png";
 
+import bannerImage from "./dummy/food-banner.jpg";
+
 export default function App() {
   return (
     <View
@@ -435,8 +437,7 @@ export default function App() {
                 paddingTop: 16,
                 paddingBottom: 20,
                 borderBottomColor: "#E8E9ED",
-                borderBottomWidth: 1,
-                marginBottom: 20
+                borderBottomWidth: 1
               }}
             >
               <Text
@@ -447,7 +448,8 @@ export default function App() {
               <Text
                 style={{ fontSize: 14, color: "#7A7A7A", marginBottom: 11 }}
               >
-                Dimas drajat selamatkan penalti, Timnas U-23 kalahkan Malaysia.
+                Dimas drajat selamatkan penalti, Timnas U-23 kalahkan Malaysia
+                dalam laga persahabatan.
               </Text>
               <TouchableOpacity
                 style={{
@@ -470,6 +472,110 @@ export default function App() {
                 </Text>
               </TouchableOpacity>
             </View>
+          </View>
+          {/* go food banner */}
+          <View style={{ padding: 16 }}>
+            <View
+              style={{
+                position: "relative"
+              }}
+            >
+              <Image
+                source={bannerImage}
+                style={{ height: 170, width: "100%", borderRadius: 6 }}
+              ></Image>
+              <View
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  backgroundColor: "black",
+                  opacity: 0.15,
+                  borderRadius: 6
+                }}
+              ></View>
+              <View
+                style={{
+                  height: 15,
+                  width: 55,
+                  position: "absolute",
+                  top: 16,
+                  left: 16
+                }}
+              >
+                <Image
+                  source={logoWhite}
+                  style={{
+                    width: undefined,
+                    height: undefined,
+                    resizeMode: "contain",
+                    flex: 1
+                  }}
+                ></Image>
+              </View>
+              <View
+                style={{
+                  position: "absolute",
+                  bottom: 0,
+                  left: 0,
+                  width: "100%",
+                  flexDirection: "row",
+                  alignItems: "center",
+                  paddingHorizontal: 16,
+                  paddingBottom: 16
+                }}
+              >
+                <View>
+                  <Text
+                    style={{
+                      fontSize: 18,
+                      fontWeight: "bold",
+                      color: "white",
+                      marginBottom: 8
+                    }}
+                  >
+                    Free GO-FOOD voucher
+                  </Text>
+                  <Text
+                    style={{ fontSize: 12, fontWeight: "500", color: "white" }}
+                  >
+                    Quick, before they run out !
+                  </Text>
+                </View>
+                <View style={{ flex: 1, paddingLeft: 12 }}>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: "#61A756",
+                      paddingHorizontal: 12,
+                      paddingVertical: 11,
+                      alignSelf: "stretch",
+                      borderRadius: 4
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 10,
+                        fontWeight: "bold",
+                        color: "white",
+                        textAlign: "center"
+                      }}
+                    >
+                      GET VOUCER
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                borderBottomColor: "#E8E9ED",
+                borderBottomWidth: 1,
+                marginTop: 16,
+                marginBottom: 20
+              }}
+            ></View>
           </View>
         </View>
       </ScrollView>
